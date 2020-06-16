a29bb6ae8b6c0334cd34a986766d303a337a82bf
Tolimanoff <web@oootisa.ru>
2020-06-16 14:05:50 +0300

    Add files via upload

---
+++ HelloShell/HelloShell/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloShell
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AppShell : Shell
+    {
+		public AppShell ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ HelloShell/HelloShell/Dogs.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloShell
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Dogs : ContentPage
+	{
+		public Dogs ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
