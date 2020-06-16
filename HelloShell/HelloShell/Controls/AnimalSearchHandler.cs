b923655b0406afc9f59833f3771da236abe2f5e3
Tolimanoff <web@oootisa.ru>
2020-06-16 14:05:18 +0300

    Add files via upload

---
+++ HelloShell/HelloShell/Controls/AnimalSearchHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HelloShell.Models;
+using Xamarin.Forms;
+
+namespace HelloShell.Controls
+{
+    public class AnimalSearchHandler : SearchHandler
+    {
+        public IList<Animal> Animals { get; set; }
+
+        protected override void OnQueryChanged(string oldValue, string newValue)
+        {
+            base.OnQueryChanged(oldValue, newValue);
+
+            if (string.IsNullOrWhiteSpace(newValue))
+            {
+                ItemsSource = null;
+            }
+            else
+            {
+                ItemsSource = Animals
+                    .Where(animal => animal.Name.ToLower().Contains(newValue.ToLower()))
+                    .ToList();
+            }
+        }
+    }
+}
